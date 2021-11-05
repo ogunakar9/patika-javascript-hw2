b5fccdd76375e1916ed8c29e261779c9d6f1fa2d
ogunakar9 <ogunakar09@gmail.com>
2021-11-05 22:03:40 +0300

    add close buttons to list items

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const myNodelist = document.getElementsByTagName("li");
+for (let i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+
